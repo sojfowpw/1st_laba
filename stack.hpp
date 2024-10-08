9546894c3a88a7a3430b4658fa3f927817b1fe02
sojfowpw <alexandra.plisova@gmail.com>
2024-10-08 15:11:13 -0400

    update

--- stack.hpp
+++ stack.hpp
@@ -1,72 +1,34 @@
 #pragma once
-#include <iostream>
-#include <string>
+
+#include "singly linked list.hpp"
 
 using namespace std;
 // стэк
-struct sNode { // узел связного списка
-    string item; // значение узла
-    sNode* next; // указатель на следующий узел
-
-    sNode(string _item) : item(_item), next(nullptr) {} // конструктор для создания нового узла с заданным значением
-};
 
 struct Stack {
-    sNode* first; // указатель на 1 узел
-    sNode* last; // указатель на последний узел
-    Stack() : first(nullptr), last(nullptr) {} // конструктор для создания пустого списка
-
-    bool is_empty() { // проверка на пустоту
-        return first == nullptr; // если первый указатель указывает на nullptr, список пуст
-    }
+    singlyList stackList; // подключаем команды односвязного листа
 
     void push(string _item) { // добавление элемента в начало
-        sNode* newNode = new sNode(_item); // создаём новый узел с заданным значением
-        if (is_empty()) { // если список пустой
-            first = newNode; // новый узел становится первым
-            last = newNode; // и последним
-            return;
-        }
-        newNode->next = first; // иначе новый узел привязывается к старому первому элементу
-        first = newNode; // новый узел становится первым
+        stackList.addToHead(_item);
     }
 
     void pop() { // удаление узла с головы
-        if (is_empty()) { // если стэк пуст
-            cout << "Стэк пуст.\n";
-            return;
-        }
-        sNode* newNode = first; // новый узел становится первым
-        first = newNode->next; // голова теперь указывает на второй узел
-        delete newNode; // удаляем старый первый узел
+        stackList.delFromHead();
     }
 
     void findIndex(int index) { // поиск элемента по индексу
-        if (is_empty()) { // проверка на пустоту
-            cout << "Стэк пуст.\n";
-            return;
-        }
-        sNode* newNode = first; // новый указатель на голову
-        for (int i = 0; i < index && newNode; i++) { // проходимя по списку, пока текущий индекс не будет равен заданному
-            newNode = newNode->next;
-        }
-        if (!newNode) { // если элемент не найден
-            cout << "Такого элемента нет в стэке.\n";
-            return;
-        }
-        cout << "Элемент по индексу: " << newNode->item << endl; // возвращаем искомый элемент
+        stackList.findIndex(index);
     }
 
     void printStack() { // вывод всех элементов стэка (голова слева)
-        if (is_empty()) { // проверка на пустоту
-            cout << "Стэк пуст.\n";
-            return;
-        }
-        sNode* newNode = first; // начинаем с первого узла
-        while (newNode) { // пока указатель не указывает на nullptr
-            cout << newNode->item << " "; // выводим значения стэка
-            newNode = newNode->next; // проходимся по узлам
-        }
-        cout << endl;
+        stackList.printList();
+    }
+
+    void loadFromFile (const string& file) { // загрузка данных из файла
+        stackList.loadFromFile(file);
+    }
+
+    void saveToFile (const string& file) { // сохранение в файл
+        stackList.saveToFile(file);
     }
 };
